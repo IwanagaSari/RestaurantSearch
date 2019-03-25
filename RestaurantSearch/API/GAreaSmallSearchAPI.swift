430c941645ce38b10a7a770e99f0ad97814bf5fc
岩永 彩里 <>
2019-03-25 20:47:51 +0900

    GAreaSmallSearchAPIの構造体を作成

--- RestaurantSearch/API/GAreaSmallSearchAPI.swift
+++ RestaurantSearch/API/GAreaSmallSearchAPI.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct AreaS: Codable {
+    let areacodeS: String
+    let areanameS: String
+    let gareaLarge: [AreaL]?
+    
+    struct AreaL: Codable {
+        let areacodeL: String
+        let areanameL: String
+    }
+        
+    enum Codingkeys1: String, CodingKey {
+        case gareaLarge = "garea_large"
+        case areacodeS = "areacode_s"
+        case areanameS = "areaname_s"
+    }
+    
+    enum Codingkeys2: String, CodingKey {
+        case areacodeL = "areacode_l"
+        case areanameL = "areaname_l"
+    }
+}
