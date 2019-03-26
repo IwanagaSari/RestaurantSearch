ab6873cde14306153d03cd570caff790f7ba6ea8
岩永彩里 <catherinrin@gmail.com>
2019-03-26 15:44:07 +0900

    AreaSearchAPITestsファイルを作成

---
+++ RestaurantSearchTests/AreaSearchAPITests.swift
@@ -0,0 +1,14 @@
+//
+//  AreaSearchAPITests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永彩里 on 2019/03/26.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+class AreaSearchAPITests: XCTestCase {
+
+}
