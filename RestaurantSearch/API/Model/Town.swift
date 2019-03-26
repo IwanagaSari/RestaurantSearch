498f9cc2676b51cee3239e7d9dbca9685f1263e0
岩永 彩里 <>
2019-03-26 19:17:42 +0900

    Town.swfitのコーディングキーを修正

--- RestaurantSearch/API/Model/Town.swift
+++ RestaurantSearch/API/Model/Town.swift
@@ -12,21 +12,21 @@ import Foundation
 struct Town: Codable {
     let areacodeS: String
     let areanameS: String
-    let gareaLarge: [AreaL]?
+    let gareaLarge: AreaL
     
     struct AreaL: Codable {
         let areacodeL: String
         let areanameL: String
+        
+        enum Codingkeys: String, CodingKey {
+            case areacodeL = "areacode_l"
+            case areanameL = "areaname_l"
+        }
     }
         
-    enum Codingkeys1: String, CodingKey {
+    enum Codingkeys: String, CodingKey {
         case gareaLarge = "garea_large"
         case areacodeS = "areacode_s"
         case areanameS = "areaname_s"
-    }
-    
-    enum Codingkeys2: String, CodingKey {
-        case areacodeL = "areacode_l"
-        case areanameL = "areaname_l"
     }
 }
