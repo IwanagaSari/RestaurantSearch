7b53318671ff3a8f2f86e214d14e4c838ebfbee3
岩永 彩里 <>
2019-03-26 18:09:51 +0900

    CodingKeyの記述位置を修正

--- RestaurantSearch/API/GAreaLargeSearchAPI.swift
+++ RestaurantSearch/API/GAreaLargeSearchAPI.swift
@@ -17,16 +17,16 @@ struct City: Codable {
     struct Pref: Codable {
         let prefCode: String
         let prefName: String
+        
+        enum CodingKeys: String, CodingKey {
+            case prefCode = "pref_code"
+            case prefName = "pref_name"
+        }
     }
     
     enum CodingKeys1: String, CodingKey {
         case areacodeL = "areacode_l"
         case areanameL = "areaname_l"
         case pref
-    }
-    
-    enum CodingKeys2: String, CodingKey {
-        case prefCode = "pref_code"
-        case prefName = "pref_name"
     }
 }
