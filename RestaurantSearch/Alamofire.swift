5829f194491aba2a5a09e5fdd0cbac6b870e994d
岩永 彩里 <>
2019-03-28 21:08:17 +0900

    Alamofireのバージョン変えて、ファイル作成した

---
+++ RestaurantSearch/Alamofire.swift
@@ -0,0 +1,19 @@
+//
+//  Alamofire.swift
+//  RestaurantSearch
+//
+//  Created by 岩永 彩里 on 2019/03/28.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Alamofire: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+    
+}
