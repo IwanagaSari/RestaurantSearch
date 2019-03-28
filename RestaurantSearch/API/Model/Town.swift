56094d4c01ba41b3d0e317c831a73855c03a49c2
岩永彩里 <catherinrin@gmail.com>
2019-03-28 13:49:44 +0900

    TownTestsファルを作成し、テストコードを追加（だがテストが通らない）

--- RestaurantSearch/API/Model/Town.swift
+++ RestaurantSearch/API/Model/Town.swift
@@ -13,20 +13,20 @@ struct Town: Codable {
     let areacodeS: String
     let areanameS: String
     let gareaLarge: AreaL
-    
+
     struct AreaL: Codable {
         let areacodeL: String
         let areanameL: String
-        
+
         enum Codingkeys: String, CodingKey {
             case areacodeL = "areacode_l"
             case areanameL = "areaname_l"
         }
     }
-        
+
     enum Codingkeys: String, CodingKey {
-        case gareaLarge = "garea_large"
         case areacodeS = "areacode_s"
         case areanameS = "areaname_s"
+        case gareaLarge = "garea_large"
     }
 }
---
+++ RestaurantSearchTests/TownTests.swift
@@ -0,0 +1,39 @@
+//
+//  TownTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永彩里 on 2019/03/28.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+final class TownTests: XCTestCase {
+    
+    func testDecode() throws {
+        let json = """
+  {
+    "areacode_s": "AREAS5502",
+    "areaname_s": "札幌駅",
+    "garea_middle": {
+        "areacode_m": "AREAM5502",
+        "areaname_m": "札幌駅"
+    },
+    "garea_large": {
+        "areacode_l": "AREAL5500",
+        "areaname_l": "札幌駅・大通・すすきの"
+    },
+    "pref": {
+        "pref_code": "PREF01",
+        "pref_name": "北海道"
+    }
+  }
+"""
+        let town = try JSONDecoder().decode(Town.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(town.areacodeS, "AREAS5502")
+        XCTAssertEqual(town.areanameS, "札幌駅")
+        XCTAssertEqual(town.gareaLarge.areacodeL, "AREAL5500")
+        XCTAssertEqual(town.gareaLarge.areanameL, "札幌駅・大通・すすきの")
+    }
+}
