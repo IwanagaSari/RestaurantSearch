2c10e8f18acc0f2e7fd3255106d0255efe4aae62
岩永彩里 <catherinrin@gmail.com>
2019-04-03 17:38:40 +0900

    試しにAPIを叩いてみた

--- RestaurantSearch/AlamofireViewController.swift
+++ RestaurantSearch/AlamofireViewController.swift
@@ -6,14 +6,27 @@
 //  Copyright © 2019年 岩永 彩里. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Alamofire
 
 class AlamofireViewController: UIViewController {
     
     override func viewDidLoad() {
-        
-        
+        let parameters = [
+            "keyid": "9e168ecbfa31f841eb3a8bc16045a424"
+        ]
+        Alamofire.request("https://api.gnavi.co.jp/master/AreaSearchAPI/v3/", parameters: parameters).responseJSON { response in
+            print("Request: \(String(describing: response.request))")
+            print("Response: \(String(describing: response.response))")
+            print("Result: \(response.result)")                         
+            
+            switch response.result {
+            case .success(let response):
+                print("成功：\(response)")
+            case .failure(let err):
+                print("失敗：\(err)")
+            }
+        }
     }
     
 }
