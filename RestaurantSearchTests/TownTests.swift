2a50132abdff29ddd5bbdea81d0c9bd266c3359b
岩永彩里 <catherinrin@gmail.com>
2019-04-05 16:08:11 +0900

    TownResponseBody のテストと、Townのテストを分けた

--- RestaurantSearchTests/TownTests.swift
+++ RestaurantSearchTests/TownTests.swift
@@ -11,7 +11,33 @@ import XCTest
 
 final class TownTests: XCTestCase {
     
-    func testDecode() throws {
+    func testTownDecode() throws {
+        let json = """
+{
+    "areacode_s": "AREAS5502",
+    "areaname_s": "札幌駅",
+    "garea_middle": {
+        "areacode_m": "AREAM5502",
+        "areaname_m": "札幌駅"
+    },
+    "garea_large": {
+        "areacode_l": "AREAL5500",
+        "areaname_l": "札幌駅・大通・すすきの"
+    },
+    "pref": {
+        "pref_code": "PREF01",
+        "pref_name": "北海道"
+    }
+}
+"""
+        let town = try JSONDecoder().decode(Town.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(town.areacodeS, "AREAS5502")
+        XCTAssertEqual(town.areanameS, "札幌駅")
+        XCTAssertEqual(town.gareaLarge.areacodeL, "AREAL5500")
+        XCTAssertEqual(town.gareaLarge.areanameL, "札幌駅・大通・すすきの")
+    }
+    
+    func testTownResponseBodyDecode() throws {
         let json = """
 {
     "@attributes": {
@@ -38,10 +64,7 @@ final class TownTests: XCTestCase {
 }
 """
         let body = try JSONDecoder().decode(TownResponseBody.self, from: json.data(using: .utf8)!)
-        let town = body.gareaSmall[0]
-        XCTAssertEqual(town.areacodeS, "AREAS5502")
-        XCTAssertEqual(town.areanameS, "札幌駅")
-        XCTAssertEqual(town.gareaLarge.areacodeL, "AREAL5500")
-        XCTAssertEqual(town.gareaLarge.areanameL, "札幌駅・大通・すすきの")
+        let townCount = body.gareaSmall.count
+        XCTAssertEqual(townCount, 1)
     }
 }
