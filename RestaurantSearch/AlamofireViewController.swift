c2a779716a18a2d50cf40c66779f7c671181eed8
岩永彩里 <catherinrin@gmail.com>
2019-04-10 14:19:05 +0900

    市のメソッド、町のメソッドを追加してAPIから情報を取得

--- RestaurantSearch/AlamofireViewController.swift
+++ RestaurantSearch/AlamofireViewController.swift
@@ -13,6 +13,8 @@ class AlamofireViewController: UIViewController {
     
     var areaResponseBody: AreaResponseBody?
     var prefectureResponseBody: PrefectureResponseBody?
+    var cityResponseBody: CityResponseBody?
+    var townResponseBody: TownResponseBody?
     var error: Error?
     let parameters = [
         "keyid": "9e168ecbfa31f841eb3a8bc16045a424"
@@ -35,6 +37,28 @@ class AlamofireViewController: UIViewController {
             self.prefectureResponseBody = prefectureResponseBody
             if let pref = prefectureResponseBody?.pref[0] {
                 print("都道府県の名前：\(pref.prefName)")
+            }
+        }, failure: { error in
+            if let error = error {
+                print(error)
+            }
+        })
+        
+        getCityAPI(success: { cityResponseBody in
+            self.cityResponseBody = cityResponseBody
+            if let city = cityResponseBody?.garea_large[0] {
+                print("市の名前：\(city.areanameL)")
+            }
+        }, failure: { error in
+            if let error = error {
+                print(error)
+            }
+        })
+        
+        getTownAPI(success: { townResponseBody in
+            self.townResponseBody = townResponseBody
+            if let town = townResponseBody?.garea_small[0] {
+                print("町の名前：\(town.areanameS)")
             }
         }, failure: { error in
             if let error = error {
@@ -69,6 +93,18 @@ class AlamofireViewController: UIViewController {
     /// 都道府県の取得
     func getPrefectureAPI(success: @escaping (PrefectureResponseBody?) -> Void, failure: @escaping (Error?) -> Void) {
         let url = "https://api.gnavi.co.jp/master/PrefSearchAPI/v3/"
+        fetchresponse(url: url, success: success, failure: failure)
+    }
+    
+    /// 市の取得
+    func getCityAPI(success: @escaping (CityResponseBody?) -> Void, failure: @escaping (Error?) -> Void) {
+        let url = "https://api.gnavi.co.jp/master/GAreaLargeSearchAPI/v3/"
+        fetchresponse(url: url, success: success, failure: failure)
+    }
+    
+    /// 町の取得
+    func getTownAPI(success: @escaping (TownResponseBody?) -> Void, failure: @escaping (Error?) -> Void) {
+        let url = "https://api.gnavi.co.jp/master/GAreaSmallSearchAPI/v3/"
         fetchresponse(url: url, success: success, failure: failure)
     }
 }
