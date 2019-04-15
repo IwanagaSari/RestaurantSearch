0611208d97e11c73f5b04db332b4394bc2d2c562
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-04-15 21:21:41 +0900

    AlamofireViewControllerTestsファイルを作成

---
+++ RestaurantSearchTests/AlamofireViewControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  AlamofireViewControllerTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永 彩里 on 2019/04/15.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+import OHHTTPStubs
+@testable import RestaurantSearch
+
+class AlamofireViewControllerTests: XCTestCase {
+
+    func testExample() {
+        
+    }
+
+}
