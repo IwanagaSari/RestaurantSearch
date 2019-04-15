247b7dd5e008ab15ea812915edf27cf4cffdd316
岩永彩里 <catherinrin@gmail.com>
2019-04-15 12:35:58 +0900

    UI/Shop以下のディレクトリ内のファイルの不要な記述を削除し、final classへ変更

--- RestaurantSearch/UI/Shop/ShopInfo/ShopInfoViewController.swift
+++ RestaurantSearch/UI/Shop/ShopInfo/ShopInfoViewController.swift
@@ -8,23 +8,10 @@
 
 import UIKit
 
-class ShopInfoViewController: UIViewController {
+final class ShopInfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
