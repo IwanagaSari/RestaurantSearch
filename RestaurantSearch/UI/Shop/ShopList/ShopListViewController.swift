fe986ac111bc4b81399be7d1d830a1ade4a0d701
岩永彩里 <catherinrin@gmail.com>
2019-04-22 16:20:03 +0900

    CollectionViewに画像を表示させた

--- RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
+++ RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-final class ShopListViewController: UIViewController {
+final class ShopListViewController: UIViewController, UICollectionViewDataSource,
+UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let photos = ["2", "3"]
 
+    override func viewDidLoad() {
+        
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .lightGray
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width:self.view.frame.width/2 - 5, height: self.view.frame.width/2 - 5)
+        collectionView.collectionViewLayout = layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
+                                               for: indexPath)
+        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+        let cellImage = UIImage(named: photos[indexPath.row])
+        imageView.image = cellImage
+        
+        let label = testCell.contentView.viewWithTag(2) as! UILabel
+        label.text = "店の名前"
+        
+        return testCell
+    }
+    
 }
