59b045520ec020eb31a463f218dc6922fb9548b7
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-04-25 17:38:18 +0900

    軽微な修正（変数名の頭文字を小文字で統一した）

--- RestaurantSearch/UI/Search/TownSelect/TownSelectViewController.swift
+++ RestaurantSearch/UI/Search/TownSelect/TownSelectViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 final class TownSelectViewController: UIViewController {
-    @IBOutlet weak var CityLabel: UIView!
+    @IBOutlet weak var cityLabel: UIView!
     @IBOutlet weak var tableView: UITableView!
     
 }
