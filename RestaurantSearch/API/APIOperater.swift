fbf6a0532bb4f40ce0917ca86d89bafe41d8db01
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-09 18:50:36 +0900

    APIOperater.swiftファイルを作成

---
+++ RestaurantSearch/API/APIOperater.swift
@@ -0,0 +1,14 @@
+//
+//  APIOperater.swift
+//  RestaurantSearch
+//
+//  Created by 岩永 彩里 on 2019/05/09.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIOperater {
+    
+}
