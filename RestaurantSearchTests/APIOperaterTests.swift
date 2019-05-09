a79e039dc425d2f78e98507a40b59a1e0dd8f3cb
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-09 18:57:51 +0900

    APIOperaterTestsファイルを作成

---
+++ RestaurantSearchTests/APIOperaterTests.swift
@@ -0,0 +1,14 @@
+//
+//  APIOperaterTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永 彩里 on 2019/05/09.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+final class APIOperaterTests: XCTestCase {
+
+}
