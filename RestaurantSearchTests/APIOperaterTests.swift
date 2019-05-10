acdd9f299194169e255937105586b97f5b5525d7
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-10 17:38:59 +0900

    不要にインスタンス変数にデータを保存していたので、削除した。

--- RestaurantSearchTests/APIOperaterTests.swift
+++ RestaurantSearchTests/APIOperaterTests.swift
@@ -11,14 +11,9 @@ import XCTest
 
 final class APIOperaterTests: XCTestCase {
     let apiOperater = APIOperater()
-    var areaResponseBody: AreaResponseBody?
-    var prefectureResponseBody: PrefectureResponseBody?
-    var cityResponseBody: CityResponseBody?
-    var townResponseBody: TownResponseBody?
     
     func testGetAreaAPI() {
         apiOperater.getAreaAPI(success: { areaResponseBody in
-            self.areaResponseBody = areaResponseBody
             XCTAssertEqual("北海道", areaResponseBody?.area[0].areaName)
         }, failure: { error in
 
@@ -27,7 +22,6 @@ final class APIOperaterTests: XCTestCase {
     
     func testGetPrefectureAPI() {
         apiOperater.getPrefectureAPI(success: { prefectureResponseBody in
-            self.prefectureResponseBody = prefectureResponseBody
             XCTAssertEqual(prefectureResponseBody?.pref[0].prefName, "北海道")
         }, failure: { error in
             
@@ -36,7 +30,6 @@ final class APIOperaterTests: XCTestCase {
     
     func testGetCityAPI() {
         apiOperater.getCityAPI(success: { cityResponseBody in
-            self.cityResponseBody = cityResponseBody
             XCTAssertEqual(cityResponseBody?.gareaLarge[0].areanameL, "札幌駅・大通・すすきの")
         }, failure: { error in
             
@@ -45,7 +38,6 @@ final class APIOperaterTests: XCTestCase {
     
     func testGetTownAPI() {
         apiOperater.getTownAPI(success: { townResponseBody in
-            self.townResponseBody = townResponseBody
             XCTAssertEqual(townResponseBody?.gareaSmall[0].areanameS, "札幌駅")
         }, failure: { error in
             
