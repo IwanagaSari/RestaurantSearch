839f4730c278ab1b9f60a51ec1163420a23ea5bf
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-13 19:46:51 +0900

    APIを叩いて、エリア一覧をテーブルビューに表示されるようにした

--- RestaurantSearch/UI/Search/AreaSelect/AreaSelectViewController.swift
+++ RestaurantSearch/UI/Search/AreaSelect/AreaSelectViewController.swift
@@ -9,5 +9,30 @@
 import UIKit
 
 final class AreaSelectViewController: UITableViewController {
+    let apiOperater = APIOperater()
+    var areas: [Area] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        apiOperater.getArea(success: { areaResponseBody in
+            self.areas = areaResponseBody.area
+            self.tableView.reloadData()
+        }, failure: { error in
+            //
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AreaCell", for: indexPath)
+        let area = areas[indexPath.row]
+        cell.textLabel?.text = area.areaName
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return areas.count
+    }
 
 }
