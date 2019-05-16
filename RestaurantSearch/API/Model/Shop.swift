adf6bd5edfc3aeeb83b7da8df4295d5b69ba3702
岩永彩里 <catherinrin@gmail.com>
2019-05-16 15:27:21 +0900

    レストラン検索APIを叩いた時のレスポンス構造体を作成

--- RestaurantSearch/API/Model/Shop.swift
+++ RestaurantSearch/API/Model/Shop.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct ShopResponseBody: Codable {
+    let rest: [Shop]
+}
+
+struct Shop: Codable {
+    let name: String
+    let tel: String
+    let address: String
+    let imageUrl: Image
+    
+    struct Image: Codable {
+        let shopImage1: String
+        let shopImage2: String
+        let qrcode: String
+        
+        enum CodingKeys: String, CodingKey {
+            case shopImage1 = "shop_image1"
+            case shopImage2 = "shop_image2"
+            case qrcode
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageUrl = "image_url"
+        case tel
+        case address
+    }
+}
