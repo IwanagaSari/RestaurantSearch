f84b4f4a374a84d41c63ab8410de9c7a94fec274
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-16 20:46:59 +0900

    エリア一覧同様に、都道府県一覧が表示されるようにした

--- RestaurantSearch/UI/Search/PrefectureSelect/PrefectureSelectViewController.swift
+++ RestaurantSearch/UI/Search/PrefectureSelect/PrefectureSelectViewController.swift
@@ -9,5 +9,39 @@
 import UIKit
 
 final class PrefectureSelectViewController: UITableViewController {
+    private let apiOperater: APIType = APIOperater()
+    private var prefectures: [Prefecture] = []
+    var prefName: String = ""
+    var areaName: String = ""
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = areaName
+        
+        apiOperater.getPrefecture(success: { [weak self] prefectureResponseBody in
+            self?.showPrefecture(prefectureResponseBody)
+        }, failure: { error in
+            self.showError(error)
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PrefectureCell", for: indexPath)
+        cell.textLabel?.text = prefectures[indexPath.row].prefName
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectures.count
+    }
+    
+    func showPrefecture(_ prefectureResponseBody: PrefectureResponseBody) {
+        self.prefectures = prefectureResponseBody.pref
+        self.tableView.reloadData()
+    }
+    
+    func showError(_ error: Error) {
+        print(error) //とりあえず
+    }
 }
