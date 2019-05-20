a76aa2c247deeb4fd4b33d3cfd1c9509e2b4592c
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-20 21:07:23 +0900

    エリア選択するとAreaSelectViewに遷移するようにした。Town選択あとは、Townの名前が表示されるようにした。

--- RestaurantSearch/UI/Search/SearchTop/SearchTopTableViewController.swift
+++ RestaurantSearch/UI/Search/SearchTop/SearchTopTableViewController.swift
@@ -15,12 +15,32 @@ final class SearchTopTableViewController: UITableViewController, UITextFieldDele
     @IBOutlet weak var sceneLabel: UILabel!
     var areaInfo: Town?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let areaInfo = areaInfo {
+            areaLabel.text = areaInfo.areanameS
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 1:
+            performSegue(withIdentifier: "toAreaSelect", sender: self)
+        default:
+            return
+        }
+    }
+    
     // MARK: - Actions
 
     @IBAction func tapView(_ sender: UITapGestureRecognizer) {
         freewordSearchBar.resignFirstResponder()
     }
-
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        print("タップされたよ")
+    }
+    
     // MARK: - UITextFieldDelegate
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
