cd4260ec376bbc59891950023c43674095272e36
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-20 22:15:37 +0900

    固定のパラメーターから画像と店の名前を表示させてみた

--- RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
+++ RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 final class ShopListViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-    
+    private let apiOperater: APIType = APIOperater()
+    var shop: [Shop] = []
     let photos = ["2", "3"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getShop()
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return shop.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -21,11 +28,18 @@ final class ShopListViewController: UICollectionViewController, UICollectionView
             collectionView.dequeueReusableCell(withReuseIdentifier: "ShopListCell",
                                                for: indexPath)
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
-        let cellImage = UIImage(named: photos[indexPath.row])
-        imageView.image = cellImage
+        let imageURL = URL(string: shop[indexPath.row].imageUrl.shopImage1)!
+        
+        fetchImage(url: imageURL, completion: { shopImage, error in
+            if error != nil {
+                print("error")
+            } else {
+                imageView.image = shopImage
+            }
+        })
         
         let label = cell.contentView.viewWithTag(2) as! UILabel
-        label.text = "店の名前"
+        label.text = shop[indexPath.row].name
         
         return cell
     }
@@ -37,5 +51,33 @@ final class ShopListViewController: UICollectionViewController, UICollectionView
         let width = (collectionView.frame.width - layout.minimumInteritemSpacing) / 2
         
         return CGSize(width: width, height: width)
+    }
+    
+    func getShop() {
+        apiOperater.getShop(areacodeS: "AREAS5504", success: { shopResponseBody in
+            self.showShop(shopResponseBody)
+        }, failure: { error in
+            
+        })
+    }
+    
+    func showShop(_ shopResponseBody: ShopResponseBody) {
+        self.shop = shopResponseBody.shop
+        self.collectionView.reloadData()
+    }
+    
+    func fetchImage(url: URL, completion: @escaping ((UIImage?, Error?) -> Void)) -> URLSessionTask? {
+        // キャッシュに保存されていないとする
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+            if let data = data {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async { completion(image, nil) }
+                }
+            } else {
+                DispatchQueue.main.async { completion(nil, error) }
+            }
+        })
+        task.resume()
+        return task
     }
 }
