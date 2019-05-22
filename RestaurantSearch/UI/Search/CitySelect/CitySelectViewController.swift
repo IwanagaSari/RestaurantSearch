12252e9a53af554d8bca6ec5a2435d59bb181188
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-22 20:42:37 +0900

    市一覧が表示されるようコードを追加

--- RestaurantSearch/UI/Search/CitySelect/CitySelectViewController.swift
+++ RestaurantSearch/UI/Search/CitySelect/CitySelectViewController.swift
@@ -9,5 +9,56 @@
 import UIKit
 
 final class CitySelectViewController: UITableViewController {
+    private let apiOperater: APIType = APIOperater()
+    private var city: [City] = []
+    private var prefecture: Prefecture!
     
+    static func instantiate(prefecture: Prefecture) -> CitySelectViewController {
+        let vc = UIStoryboard(name: "PrefectureSelect", bundle: nil).instantiateInitialViewController() as! CitySelectViewController
+        vc.prefecture = prefecture
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = prefecture.prefName
+        
+        getCity()
+    }
+    
+    private func getCity() {
+        apiOperater.getCity(success: { [weak self] cityResponseBody in
+            self?.showCity(cityResponseBody)
+            }, failure: { error in
+                self.showError(error)
+        })
+    }
+    
+    private func showCity(_ cityResponseBody: CityResponseBody) {
+        for data in cityResponseBody.city {
+            if prefecture.prefName == data.pref.prefName {
+                city.append(data)
+            }
+        }
+        self.tableView.reloadData()
+    }
+    
+    private func showError(_ error: Error) {
+        print(error) //とりあえず
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        cell.textLabel?.text = city[indexPath.row].cityName
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return city.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toTownSelect", sender: self)
+    }
 }
