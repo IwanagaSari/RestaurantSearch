945cbd96801edb586b2ff02da59d293498cc6b12
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-22 21:14:44 +0900

    町一覧が表示されるようコードを追加。ただなぜか表示されない。

--- RestaurantSearch/UI/Search/TownSelect/TownSelectViewController.swift
+++ RestaurantSearch/UI/Search/TownSelect/TownSelectViewController.swift
@@ -9,5 +9,57 @@
 import UIKit
 
 final class TownSelectViewController: UITableViewController {
-
+    private let apiOperater: APIType = APIOperater()
+    private var town: [Town] = []
+    private var city: City!
+    
+    static func instantiate(city: City) -> TownSelectViewController {
+        let vc = UIStoryboard(name: "TownSelect", bundle: nil).instantiateInitialViewController() as! TownSelectViewController
+        vc.city = city
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = city.cityName
+        
+        getTown()
+    }
+    
+    private func getTown() {
+        apiOperater.getTown(success: { [weak self] townResponseBody in
+            self?.showTown(townResponseBody)
+            }, failure: { error in
+                self.showError(error)
+        })
+    }
+    
+    private func showTown(_ townResponseBody: TownResponseBody) {
+        for data in townResponseBody.town {
+            if city.cityName == data.city.cityCode {
+                town.append(data)
+            }
+        }
+        self.tableView.reloadData()
+    }
+    
+    private func showError(_ error: Error) {
+        print(error) //とりあえず
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TownCell", for: indexPath)
+        cell.textLabel?.text = town[indexPath.row].townName
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return town.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = SearchTopTableViewController()
+        show(vc, sender: nil)
+    }
 }
