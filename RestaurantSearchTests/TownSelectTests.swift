4194aa3dc9d6a42b7668c5baa5ad421e50066ad0
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-27 17:27:57 +0900

    TownSelectのテストコードを追加

--- RestaurantSearchTests/TownSelectTests.swift
+++ RestaurantSearchTests/TownSelectTests.swift
@@ -7,6 +7,15 @@
 //
 
 import XCTest
+@testable import RestaurantSearch
 
 class TownSelectTests: XCTestCase {
+    
+    func testInitialize() {
+        let city = City(cityCode: "222", cityName: "福岡市", pref: City.Pref(prefCode: "111",
+                                                                            prefName: "福岡県"))
+        let vc: TownSelectViewController!
+        vc = TownSelectViewController.instantiate(city: city)
+        XCTAssertNotNil(vc)
+    }
 }
