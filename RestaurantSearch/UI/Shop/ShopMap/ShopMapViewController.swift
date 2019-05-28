5c3da1570a2f624cc7cf797a7624b49c5cc8a39a
岩永彩里 <catherinrin@gmail.com>
2019-05-28 16:40:31 +0900

    textViewに住所を反映させた

--- RestaurantSearch/UI/Shop/ShopMap/ShopMapViewController.swift
+++ RestaurantSearch/UI/Shop/ShopMap/ShopMapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 final class ShopMapViewController: UIViewController {
-    @IBOutlet weak private var shopAdressLabel: UILabel!
+    @IBOutlet weak var shopAdressLabel: UITextView!
     @IBOutlet weak private var shopMapView: MKMapView!
     private var shop: Shop!
     
@@ -18,5 +18,11 @@ final class ShopMapViewController: UIViewController {
         let vc = UIStoryboard(name: "ShopMap", bundle: nil).instantiateInitialViewController() as! ShopMapViewController
         vc.shop = shop
         return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        shopAdressLabel.text = shop.address
     }
 }
