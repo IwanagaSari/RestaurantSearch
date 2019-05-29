575581e5ad31f14ceff7fb38feb10afacbfc8a6c
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-29 20:13:02 +0900

    不要な場所でAlamofireをインポートしてたので、削除した

--- RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
+++ RestaurantSearch/UI/Shop/ShopList/ShopListViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Alamofire
 
 final class ShopListViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     private let apiOperater = APIOperater()
