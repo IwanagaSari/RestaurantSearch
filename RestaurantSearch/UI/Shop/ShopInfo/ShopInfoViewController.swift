154cabcf73d8b56e87f1eab80d6fa9293db337b7
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-29 18:44:46 +0900

    不要な場所でAlamofireをインポートしてたので、削除した

--- RestaurantSearch/UI/Shop/ShopInfo/ShopInfoViewController.swift
+++ RestaurantSearch/UI/Shop/ShopInfo/ShopInfoViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SafariServices
-import Alamofire
 
 final class ShopInfoViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
