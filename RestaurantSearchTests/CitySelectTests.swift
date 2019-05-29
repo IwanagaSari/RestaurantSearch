2811b8b400a24cc936c84c031b6cc90cdeb13ce0
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-29 21:50:57 +0900

    dummyAPIを使って、City一覧が表示させるかどうか、のテストを追加

--- RestaurantSearchTests/CitySelectTests.swift
+++ RestaurantSearchTests/CitySelectTests.swift
@@ -10,10 +10,24 @@ import XCTest
 @testable import RestaurantSearch
 
 final class CitySelectTests: XCTestCase {
+    private let dummyAPI = DummyAPI()
     
     func testInstantiate() {
         let prefecture = Prefecture(prefCode: "111", prefName: "福岡県", areaCode: "000")
         let vc = CitySelectViewController.instantiate(prefecture: prefecture)
         XCTAssertNotNil(vc)
+    }
+    
+    func testShowAreaList() {
+        let prefecture = Prefecture(prefCode: "111", prefName: "福岡県", areaCode: "000")
+        let vc = CitySelectViewController.instantiate(prefecture: prefecture)
+        vc.apiOperater = dummyAPI
+        vc.loadViewIfNeeded()
+        
+        let number = vc.tableView.numberOfRows(inSection: 0)
+        XCTAssertEqual(number, 1)
+        
+        let cell = vc.tableView.cellForRow(at: IndexPath(row: 0, section: 0))!
+        XCTAssertEqual(cell.textLabel?.text, "大濠・六本松・桜坂")
     }
 }
