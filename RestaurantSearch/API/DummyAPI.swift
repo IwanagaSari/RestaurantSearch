752fbfea020bc91dd9505162c7dc5cc886db58bd
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-05-29 18:33:13 +0900

    DummyAPIもレスポンスを追加した

--- RestaurantSearch/API/DummyAPI.swift
+++ RestaurantSearch/API/DummyAPI.swift
@@ -22,11 +22,18 @@ class DummyAPI: APIType {
                                             city: Town.AreaL(cityCode: "666",
                                                                          cityName: "大濠・六本松・桜坂"))])
     var shop = ShopResponseBody(shop: [Shop(name: "店の名前",
-                                            tel: "092-642-6900",
-                                            address: "福岡市東区馬出",
-                                            imageUrl: Shop.Image(shopImage1: "https://uds.gnst.jp/rest/img/e0p4w8tb0000/t_0n5s.png",
-                                                                 shopImage2: "",
-                                                                 qrcode: "https://c-r.gnst.jp/tool/qr/?id=h598811&q=6"))])
+                                              nameKana: "カタカナ",
+                                              latitude: "緯度",
+                                              longitude: "経度",
+                                              category: "カテゴリー",
+                                              imageUrl: Shop.Image(shopImage1: "https://uds.gnst.jp/rest/img/e0p4w8tb0000/t_0n5s.png",
+                                                                   shopImage2: "",
+                                                                   qrcode: "https://c-r.gnst.jp/tool/qr/?id=h598811&q=6"),
+                                              address: "福岡市東区馬出",
+                                              tel: "092-642-6900",
+                                              opentime: "18:00~24:00",
+                                              pr: Shop.Pr(prShort: "すっごく美味しいですよ〜"),
+                                              code: Shop.Code(genre: ["焼き鳥", "鳥料理"]))])
     
     func getArea(success: @escaping (AreaResponseBody) -> Void, failure: @escaping (Error) -> Void) {
         success(area)
