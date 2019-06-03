4a575331193497577b2591fbfb72244a6baf3ee3
岩永彩里 <catherinrin@gmail.com>
2019-06-03 11:18:46 +0900

    ShopMapのテストファイルを作成

---
+++ RestaurantSearchTests/ShopMapTests.swift
@@ -0,0 +1,14 @@
+//
+//  ShopMapTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永彩里 on 2019/06/03.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+class ShopMapTests: XCTestCase {
+
+}
