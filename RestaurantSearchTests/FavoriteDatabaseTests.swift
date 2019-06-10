51829d9c8e5957d8f54dd6090d7caa9423b9e6bf
岩永彩里 <catherinrin@gmail.com>
2019-06-10 16:48:39 +0900

    FavoriteDatabaseクラスのテストファイル作成

---
+++ RestaurantSearchTests/FavoriteDatabaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteDatabaseTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永彩里 on 2019/06/10.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+final class FavoriteDatabaseTests: XCTestCase {
+
+    func test() {
+    }
+}
