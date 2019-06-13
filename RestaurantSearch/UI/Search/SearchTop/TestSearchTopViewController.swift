7ae423b925400e2edf7260561a14e41cb567921c
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-06-13 20:15:45 +0900

    起動時の画像と、起動後の画像のサイズを統一した。テキストフィールドとエリアボタンを上部に配置した。

---
+++ RestaurantSearch/UI/Search/SearchTop/TestSearchTopViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TestSearchTopViewController.swift
+//  RestaurantSearch
+//
+//  Created by 岩永 彩里 on 2019/06/13.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+class TestSearchTopViewController: UIViewController {
+    @IBOutlet weak private var bottomView: UIView!
+    @IBOutlet weak private var freewordTextField: UITextField!
+    @IBOutlet weak private var areaSelectButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        tabBarController?.tabBar.isHidden = true
+        
+        UIView.animate(withDuration: 1.5, animations: { () -> Void in
+            self.bottomView.backgroundColor = UIColor.black// UIColor(red: 235/255, green: 235/255, blue: 235/255, alpha: 1)
+        })
+//        self.bottomView.center = self.view.center
+//        UIView.animate(withDuration: 0.5, delay: 0.0, options: .autoreverse, animations: {
+//            self.bottomView.center.y += 85.0
+//
+//        }, completion: nil)
+    }
+}
