958cfa5a2a34de612457aac0b32795155212cf49
岩永彩里 <catherinrin@gmail.com>
2019-06-13 12:01:31 +0900

    ShopInfoのImageViewをImageListCellにOutlet接続

--- RestaurantSearch/UI/Shop/ImageListCell.swift
+++ RestaurantSearch/UI/Shop/ImageListCell.swift
@@ -15,6 +15,7 @@ final class ImageListCell: UICollectionViewCell {
     var onReuse: (() -> Void)?
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var shopName: UILabel!
+    @IBOutlet weak var imageViewInShopInfo: UIImageView!
     
     override func prepareForReuse() {
         super.prepareForReuse()
