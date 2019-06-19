a2ed5c713b9cafbea68432e096d92b57695b3f9c
岩永彩里 <catherinrin@gmail.com>
2019-06-19 23:31:04 +0900

    freewordを取得・保存するコードを追加

--- RestaurantSearch/UI/Search/SearchTop/FreewordDatabase.swift
+++ RestaurantSearch/UI/Search/SearchTop/FreewordDatabase.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+protocol FreewordDatabaseType {
+    func get() -> String
+    func set(_ inputFreeword: String?)
+    func remove()
+}
+
+final class FreewordDatabase: FreewordDatabaseType {
+    private let defaults: UserDefaults
+    static let shared = FreewordDatabase(defaults: UserDefaults.standard)
+    private static let freewordKey = "freeword"
+    
+    init(defaults: UserDefaults) {
+        self.defaults = defaults
+    }
+    
+    private var freeword: String {
+        get {
+            let freeword = defaults.object(forKey: type(of: self).freewordKey) as? String
+            return freeword ?? ""
+        }
+        set {
+            defaults.set(newValue, forKey: type(of: self).freewordKey)
+            defaults.synchronize()
+        }
+    }
+    
+    func get() -> String {
+        return freeword
+    }
+    
+    func set(_ inputFreeword: String?) {
+        if let inputFreeword = inputFreeword {
+            freeword = inputFreeword
+        }
+    }
+    
+    func remove() {
+        freeword.removeAll()
+    }
+}
