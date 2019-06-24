cc8a9373fa4a2741175ee46e3bd3dd8fd361f6b8
岩永彩里 <catherinrin@gmail.com>
2019-06-24 15:35:46 +0900

    サイズを合わせるためにTopViewControllerを作成し、ナビゲーションバーとタブバーを無くすコードを追加

---
+++ RestaurantSearch/UI/Search/SearchTop/TopViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopViewController.swift
+//  RestaurantSearch
+//
+//  Created by 岩永彩里 on 2019/06/24.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        tabBarController?.tabBar.isHidden = true
+    }
+}
