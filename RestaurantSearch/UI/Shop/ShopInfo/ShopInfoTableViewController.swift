d83367466e0266b42117cb99db92c15802cc359e
岩永彩里 <catherinrin@gmail.com>
2019-06-26 20:17:26 +0900

    ShopInfoTableViewController.swiftを作成。今のShopInfoViewController.swiftファイル削除したら、ファイル名変更予定。

---
+++ RestaurantSearch/UI/Shop/ShopInfo/ShopInfoTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShopInfoTableViewController.swift
+//  RestaurantSearch
+//
+//  Created by 岩永彩里 on 2019/06/26.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+final class ShopInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
