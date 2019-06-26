062adf118643241f94bec0fdda14f133095feb31
岩永彩里 <catherinrin@gmail.com>
2019-06-26 20:10:06 +0900

    ShopPRCell.swiftファイルを作成し、xibファイルのクラスに指定した

---
+++ RestaurantSearch/UI/Shop/ShopInfo/ShopPRCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopPRCell.swift
+//  RestaurantSearch
+//
+//  Created by 岩永彩里 on 2019/06/26.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+class ShopPRCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
