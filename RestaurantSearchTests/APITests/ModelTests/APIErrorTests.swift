3354d137c6d1797f491f882e57d7562e19aba983
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-06-27 21:35:42 +0900

    APIErrorTestsファイルを作成

---
+++ RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
@@ -0,0 +1,14 @@
+//
+//  APIErrorTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永 彩里 on 2019/06/27.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantSearch
+
+final class APIErrorTests: XCTestCase {
+
+}
