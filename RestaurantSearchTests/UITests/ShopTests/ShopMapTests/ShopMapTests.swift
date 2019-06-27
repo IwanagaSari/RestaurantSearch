0cfadf47f5cefbb5f54e1b7da5df26d238b8ab09
岩永彩里 <catherinrin@gmail.com>
2019-06-27 11:49:52 +0900

    ShopMapTestsとShopInfoTestsにもレスポンスを追加(ここではなぜか\nいいらしい)

--- RestaurantSearchTests/UITests/ShopTests/ShopMapTests/ShopMapTests.swift
+++ RestaurantSearchTests/UITests/ShopTests/ShopMapTests/ShopMapTests.swift
@@ -24,7 +24,9 @@ final class ShopMapTests: XCTestCase {
                         address: "福岡市東区馬出",
                         tel: "092-642-6900",
                         opentime: "18:00~24:00",
-                        pr: Shop.Pr(prShort: "すっごく美味しいですよ〜"),
+                        holiday: "不定休日あり",
+                        pr: Shop.Pr(prShort: "すっごく美味しいですよ〜",
+                                    prLong: "今泉に11月NEWオープン!!!\n貸切宴会や飲み会、結婚式二次会にオススメ！\n・交通アクセス\n西鉄天神駅より徒歩3分\n・プラン\n地域最安値の2,500円～\n・席\n着席70名、立席120名\n・特典\n①カラオケ無料\n②新郎新婦無料"),
                         code: Shop.Code(genre: ["広島風お好み焼き", ""]))
         let vc = ShopMapViewController.instantiate(shop: shop)
         XCTAssertNotNil(vc)
