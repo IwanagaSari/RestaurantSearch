d01bb3b776d5e729a8d2ee2f8dc679ba5c08c7f9
岩永彩里 <catherinrin@gmail.com>
2019-06-28 12:09:46 +0900

    テストコードを追加

--- RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
+++ RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
@@ -10,5 +10,20 @@ import XCTest
 @testable import RestaurantSearch
 
 final class APIErrorTests: XCTestCase {
-
+    
+    func testAPIErrorDecode() throws {
+        let json = """
+{
+    "@attributes":{
+        "api_version":"v3"
+    },
+    "error":{
+        "code":"400",
+        "message":"指定されたパラメーターは存在しません"
+    }
+}
+"""
+        let body = try JSONDecoder().decode(APIErrorResponseBody.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(body.error.message, "指定されたパラメーターは存在しません")
+    }
 }
