e96a4585fcbf4a14aa26f6652776d0365dc01e5b
岩永彩里 <catherinrin@gmail.com>
2019-06-28 16:28:34 +0900

    for in 文で、Cannot assign to property: 'favorite' is a 'let' constant というエラーが出る

--- RestaurantSearch/UI/Shop/FavoriteList/FavoriteListViewController.swift
+++ RestaurantSearch/UI/Shop/FavoriteList/FavoriteListViewController.swift
@@ -55,6 +55,12 @@ final class FavoriteListViewController: UICollectionViewController, UICollection
         // ここでidが一致すればimageURLを埋めてあげる処理をする
         // わからなかったので保留
         shopList.append(shopResponseBody.shop[0])
+        for favorite in favorites {
+            if favorite.id == shopResponseBody.shop[0].id {
+                favorite.name = shopResponseBody.shop[0].name
+                favorite.imageUrl = shopResponseBody.shop[0].imageUrl.shopImage1
+            }
+        }
         collectionView.backgroundView = nil
         collectionView.reloadData()
     }
