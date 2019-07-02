a5a5311508ec90960597d1a2cdda5c17d72ef299
岩永彩里 <catherinrin@gmail.com>
2019-07-02 15:45:05 +0900

    配置したボタンの枠を角丸にした

--- RestaurantSearch/UI/Search/SearchTop/SearchTopDetailViewController.swift
+++ RestaurantSearch/UI/Search/SearchTop/SearchTopDetailViewController.swift
@@ -9,8 +9,22 @@
 import UIKit
 
 final class SearchTopDetailViewController: UITableViewController {
+    @IBOutlet weak private var freewordTextField: UITextField!
+    @IBOutlet weak private var areaSelectButton: UIButton!
+    @IBOutlet weak private var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layout()
+    }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return (tableView.frame.height - view.safeAreaInsets.top) / 3 // 3は使っているセルの数
+    }
+    
+    func layout() {
+        areaSelectButton.layer.cornerRadius = 5.0
+        searchButton.layer.cornerRadius = 5.0
     }
 }
