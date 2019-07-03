8a734811585af3e4ed14b4f1b4d8ca46355f2e92
岩永彩里 <catherinrin@gmail.com>
2019-07-03 23:56:44 +0900

    FavoriteListTestsファイルを作成

---
+++ RestaurantSearchTests/UITests/ShopTests/FavoriteListTests/FavoriteListTests.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteListTests.swift
+//  RestaurantSearchTests
+//
+//  Created by 岩永彩里 on 2019/07/03.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+
+final class FavoriteListTests: XCTestCase {
+
+}
