a0f1175f48bc920c47a0f43b339e64fde459f18c
岩永彩里 <catherinrin@gmail.com>
2019-07-10 14:17:42 +0900

    それに合わせてテストコード変更

--- RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
+++ RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
@@ -11,19 +11,33 @@ import XCTest
 
 final class APIErrorTests: XCTestCase {
     
-    func testAPIErrorDecode() throws {
+    func testAPIErrorDecode2() throws {
         let json = """
 {
-    "@attributes":{
-        "api_version":"v3"
-    },
-    "error":{
-        "code":"400",
-        "message":"指定されたパラメーターは存在しません"
-    }
+        "code": 401,
+        "message": "無効なkeyidです"
 }
 """
-        let body = try JSONDecoder().decode(APIErrorResponseBody.self, from: json.data(using: .utf8)!)
-        XCTAssertEqual(body.error.message, "指定されたパラメーターは存在しません")
+        let apiError2 = try JSONDecoder().decode(APIError2.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(apiError2.message, "無効なkeyidです")
     }
+    
+    func testAPIErrorDecode3() throws {
+        let json = """
+{
+    "@attributes": {
+        "api_version": "v3"
+    },
+    "error": [
+        {
+            "code": 401,
+            "message": "無効なkeyidです"
+        }
+    ]
+}
+"""
+        let body = try JSONDecoder().decode(APIErrorResponseBody2.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(body.error.count, 1)
+    }
+    
 }
