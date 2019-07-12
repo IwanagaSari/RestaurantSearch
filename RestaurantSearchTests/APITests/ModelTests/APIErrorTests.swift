51976bbb9f655261a8b19445ca26b8f7b6736e34
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-07-12 19:17:36 +0900

    テストコードを追加

--- RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
+++ RestaurantSearchTests/APITests/ModelTests/APIErrorTests.swift
@@ -14,6 +14,17 @@ final class APIErrorTests: XCTestCase {
     func testAPIErrorDecode() throws {
         let json = """
 {
+        "code": 401,
+        "message": "無効なkeyidです"
+}
+"""
+        let apiError2 = try JSONDecoder().decode(APIError.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(apiError2.message, "無効なkeyidです")
+    }
+    
+    func testAPIErrorResponseBodyDecode() throws {
+        let json = """
+{
     "@attributes":{
         "api_version":"v3"
     },
@@ -25,5 +36,23 @@ final class APIErrorTests: XCTestCase {
 """
         let body = try JSONDecoder().decode(APIErrorResponseBody.self, from: json.data(using: .utf8)!)
         XCTAssertEqual(body.error.message, "指定されたパラメーターは存在しません")
+    }
+    
+    func testAPIErrorResponseBody2Decode() throws {
+        let json = """
+{
+    "@attributes": {
+        "api_version": "v3"
+    },
+    "error": [
+        {
+            "code": 401,
+            "message": "無効なkeyidです"
+        }
+    ]
+}
+"""
+        let body = try JSONDecoder().decode(APIErrorResponseBody2.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(body.error.count, 1)
     }
 }
