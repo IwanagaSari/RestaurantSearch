f8f2f53ea9496fba610776688d76fff40f21caa2
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-07-22 20:47:57 +0900

    AreaSelectViewControllerのテストコードを追加

--- RestaurantSearchTests/UITests/SearchTests/AreaSelectTests/AreaSelectViewControllerTests.swift
+++ RestaurantSearchTests/UITests/SearchTests/AreaSelectTests/AreaSelectViewControllerTests.swift
@@ -10,5 +10,22 @@ import XCTest
 @testable import RestaurantSearch
 
 final class AreaSelectViewControllerTests: XCTestCase {
-
+    private let dummyAPI = DummyAPI()
+    
+    func testInstantiate() {
+        let vc = AreaSelectViewController.instantiate()
+        XCTAssertNotNil(vc)
+    }
+    
+    func testShowAreaList() {
+        let vc = AreaSelectViewController.instantiate()
+        vc.apiOperater = dummyAPI
+        vc.loadViewIfNeeded()
+        
+        let number = vc.tableView(vc.tableView, numberOfRowsInSection: 0)
+        XCTAssertEqual(number, 1)
+        
+        let cell = vc.tableView(vc.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        XCTAssertEqual(cell.textLabel?.text, "九州")
+    }
 }
