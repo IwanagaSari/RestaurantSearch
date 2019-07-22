68439d4e6d8079c0e2265838d5014f94133f3324
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-07-22 19:50:32 +0900

    ShopMapViewControllerTestsのファイル名に変更

--- RestaurantSearchTests/UITests/ShopTests/ShopMapTests/ShopMapViewControllerTests.swift
+++ RestaurantSearchTests/UITests/ShopTests/ShopMapTests/ShopMapViewControllerTests.swift
@@ -1,5 +1,5 @@
 //
-//  ShopMapTests.swift
+//  ShopMapViewControllerTests.swift
 //  RestaurantSearchTests
 //
 //  Created by 岩永彩里 on 2019/06/03.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import RestaurantSearch
 
-final class ShopMapTests: XCTestCase {
+final class ShopMapViewControllerTests: XCTestCase {
     
     func testInstantiate() {
         let shop = Shop(id: "h793032",
