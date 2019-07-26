d67e31e5811fed2862ce1ca55c7608d8ceaa4090
岩永彩里 <catherinrin@gmail.com>
2019-07-26 16:31:45 +0900

    [weak self]を削除アラートのクロージャーに追加した

--- RestaurantSearch/UI/FavoriteList/FavoriteListViewController.swift
+++ RestaurantSearch/UI/FavoriteList/FavoriteListViewController.swift
@@ -70,8 +70,8 @@ final class FavoriteListViewController: UICollectionViewController, UICollection
     
     private func showDeleteAlert(shopID: String) {
         let alertController = UIAlertController(title: "お店情報が取得できません", message: "お気に入りから削除しますか？", preferredStyle: .alert)
-        let action1 = UIAlertAction(title: "YES", style: .default, handler: { (action: UIAlertAction!) in
-            self.removeShop(shopID)
+        let action1 = UIAlertAction(title: "YES", style: .default, handler: { [weak self] _ in
+            self?.removeShop(shopID)
         })
         let action2 = UIAlertAction(title: "NO", style: .default, handler: nil)
         alertController.addAction(action1)
